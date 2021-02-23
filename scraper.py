dac93b38f23211260c05ae574b803f4f8a26fb63
Santiago Salgado <jssalgadog@gmail.com>
2021-02-23 15:11:10 -0500

    Primera version

--- scraper.py
+++ scraper.py
@@ -18,6 +18,7 @@ XPATH_EDITORIAL = '//div[@id = "metadata-editorial"]/a/text()' #constant editori
 XPATH_CATEGORY = '//div[@id = "metadata-categoría"]/text()' # constant category
 XPATH_NAME = '//div[@class = "info-libro"]//h1/text()' # constant name
 XPATH_OPINIONS = '//div[@class = "info-libro"]//a[@id ="valoracion"]/text()' # constant opinions
+XPATH_PRICE = '//div[@id = "metadata-editorial"]/a/text()'
 
 def parse_home():
     try:
@@ -47,6 +48,53 @@ def parse_home():
     except ValueError as ve:
         print(ve)
 
+
+def parsed_notice(link, date):
+    try:
+        #print(f'Se inicia parseo del link {link}')
+        response = requests.get(link)
+        if response.status_code == 200:
+            #print("Se obtuvo status code correcto")
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+            
+            try:
+                libro = ''
+                libro = parsed.xpath(XPATH_NAME)[0]                
+                print(f'{link} el titulo es {libro}')
+                autor = parsed.xpath(XPATH_AUTHOR)[0]
+                print('autor correcto')
+                precio = parsed.xpath(XPATH_PRICE)
+                print('Precio correcto')
+                editorial = parsed.xpath(XPATH_EDITORIAL)
+                print('Editorial correcto')
+                category = parsed.xpath(XPATH_CATEGORY)
+                print('Categoria correcto')
+                opinions = parsed.xpath(XPATH_OPINIONS)
+                print(title," parseado correctamente")
+            except IndexError:
+                return
+
+            with open(f'{date}/{title}.txt','w',encoding='utf-8') as f:
+                
+                f.write(libro)
+                f.write('\n\n')
+                f.write(autor)
+                f.write('\n\n')
+                f.write(precio)
+                f.write('\n\n')
+                f.write(editorial)
+                f.write('\n\n')
+                f.write(category)
+                f.write('\n\n')
+                f.write(opinions)
+                f.write('\n\n')              
+                
+
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+    except ValueError as ve:
+        print(ve)
 
 
 def main():
